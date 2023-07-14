b5f201ddb0c13b137bde9f1a914b3effe3973d41
Simone Tartaglia <96080101+startaglia@users.noreply.github.com>
2023-07-14 18:01:16 +0200

    Create inter.c

---
+++ inter/inter.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+int	ft_check_char(char *str, char c, int index)
+{
+	int	i;
+
+	i = 0;
+	while (i < index)
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_inter(char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (!ft_check_char(s1, s1[i], i))
+		{
+			j = 0;
+			while (s2[j])
+			{
+				if (s2[j] == s1[i])
+				{
+					write(1, &s1[i], 1);
+					break ;
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		ft_inter(av[1], av[2]);
+		write(1, "\n", 1);
+		return (0);
+	}
+	return (1);
+}
